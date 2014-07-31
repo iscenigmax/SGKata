02567d7444e99671122e602c24c0f81d565a6067
Carlos A. Sanchez <ing.casr@gmail.com>
2014-07-31 18:19:53 -0500

    Update football.py

--- football.py
+++ football.py
@@ -8,7 +8,14 @@ with open('football.csv', 'r') as csvfile:
     for row in spamreader:
         dic[row[0]] = int(row[5]) - int(row[6])
 
+print("Listado menor")
 sorted_x = sorted(dic.items(), key=lambda x: x[1], reverse=True)
 
 for item in sorted_x:
-    print(item[0] + ' con diferencia de ' + str(item[1]))
+    print(item[0] + ' con menor diferencia de ' + str(item[1]))
+    
+print("Listado mayor")
+sorted_x = sorted(dic.items(), key=lambda x: x[1])
+
+for item in sorted_x:
+    print(item[0] + ' con mayor diferencia de ' + str(item[1]))
